8659181e2e926a36cf946e8c90d3d1755409ddb9
Guilherme Mattos Conde <guilherme.mattos.conde@gmail.com>
2026-04-16 14:54:07 -0300

    chore: configuração inicial do projeto

---
+++ src/test/java/br/com/unifil/projeto_de_estagio_api/ProjetoDeEstagioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unifil.projeto_de_estagio_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDeEstagioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
