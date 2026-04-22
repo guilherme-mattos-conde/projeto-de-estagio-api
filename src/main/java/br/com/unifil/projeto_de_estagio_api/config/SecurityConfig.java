65f42186e53bc0d30e0d851f4181a82e52673ac4
Guilherme Mattos Conde <guilherme.mattos.conde@gmail.com>
2026-04-22 16:15:49 -0300

    chore: libera endpoints temporariamente para desenvolvimento do frontend

--- src/main/java/br/com/unifil/projeto_de_estagio_api/config/SecurityConfig.java
+++ src/main/java/br/com/unifil/projeto_de_estagio_api/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package br.com.unifil.projeto_de_estagio_api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,10 +15,10 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(withDefaults());
+                        .anyRequest().permitAll()
+                );
 
         return http.build();
     }
